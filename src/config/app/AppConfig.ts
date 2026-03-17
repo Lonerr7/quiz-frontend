c71ff90db93c717a1d7c6fc00316177f744c0437
Ivan <ivan.karabankov@gmail.com>
2026-03-17 12:56:52 +0300

    1. Updated BASE PROD URL FOR HEROKU

--- src/config/app/AppConfig.ts
+++ src/config/app/AppConfig.ts
@@ -2,4 +2,4 @@ import {__IS_DEV__} from './Environment';
 
 export const BASE_URL = __IS_DEV__
   ? 'http://localhost:8000/api/v1'
-  : 'https://quiz-backend-l1qo.onrender.com/api/v1';
+  : 'https://qiuz-backend-9ca6853878da.herokuapp.com/api/v1';
